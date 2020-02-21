8359a5729d183012a8075f01902d196d11b2bd95
Franco Caputo <FCaputo@clusterseven.com>
2020-02-21 13:37:01 +0000

    publish mods for easy querying of job method from json definition on Hangfore.Job table

--- HangfireSSDT/HangfireSSDT/HangFire/Tables/HangFire.Job.sql
+++ HangfireSSDT/HangfireSSDT/HangFire/Tables/HangFire.Job.sql
@@ -6,17 +6,22 @@
     [Arguments]      NVARCHAR (MAX) NOT NULL,
     [CreatedAt]      DATETIME       NOT NULL,
     [ExpireAt]       DATETIME       NULL,
+	JobMethod   AS( CONVERT( NVARCHAR(500), SUBSTRING( InvocationData, PATINDEX('%"Method":"%',InvocationData)+10, CHARINDEX('"',InvocationData, PATINDEX('%"Method":"%',InvocationData)+10) - (PATINDEX('%"Method":"%',InvocationData)+10) ) ) ) PERSISTED,
     CONSTRAINT [PK_HangFire_Job] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
-
-
 GO
+
+CREATE INDEX [IX_HangFire_Job_JobMethod]
+  ON [HangFire].[Job] ([JobMethod])
+  INCLUDE ([StateId], [StateName])
+  ON [PRIMARY]
+GO
+
 CREATE NONCLUSTERED INDEX [IX_HangFire_Job_StateName]
     ON [HangFire].[Job]([StateName] ASC);
-
-
 GO
+
 CREATE NONCLUSTERED INDEX [IX_HangFire_Job_ExpireAt]
     ON [HangFire].[Job]([ExpireAt] ASC)
     INCLUDE([Id]);
-
+GO
